a5db476fb3995f1ed7c1a730cc5b74a945d4d475
snozaki <tldk1993@gmail.com>
2019-10-01 08:30:56 +0900

    途中

--- binary.c
+++ binary.c
@@ -2,17 +2,39 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+ 0から255までの整数を入力させ、その数値を二進数に変換する
+ */
 int main(int argc, char *argv[])
 {
     int n = atoi(argv[1]);
     
-    if (n == 1) {
-    	printf("引数が整数ではありません。プログラムを終了します。");
+    if (n == 0) {
+    	printf("引数が整数ではありません。プログラムを終了します。\n");
+        exit(1);
     } else if (n > 255 || n < 0) {
-    	printf("引数が0以上255以下ではありません。プログラムを終了します。");
+    	printf("引数が0以上255以下ではありません。プログラムを終了します。\n");
+        exit(1);
     }
     
-    int AND = n | 0x80;
+    // (2) 0x80との論理積
+    int AND = n & 0x80;
+    printf("AND: %d\n", AND);
+    
+    if(AND == 0) {
+        printf("0");
+    } else if (AND == 1) {
+        printf("1");
+    }
+    
+    // (3) 1ビット左にシフトする
+    int m = n << 1;
+    printf("shifted: %d\n", m);
+    
+    if(m == 0){
+        printf("\n");
+        exit(0);
+    }
     
     return 0;
 }
